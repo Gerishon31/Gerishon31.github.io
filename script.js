5599180bbc04b9b1d17688aab231718d3cc602a4
Gerishon <mwaniki31@gmail.com>
2018-09-03 21:22:15 +0000

    second upload

--- script.js
+++ script.js
@@ -9,19 +9,24 @@ function mouseOut() {
 }
 
 
+
 function big(x) {
     x.style.height = "200px";
     x.style.width = "200px";
 }
-
 function normalImg(x) {
     x.style.height = "120px";
     x.style.width = "120px";
 }
 
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+function toggleDisplay5() {
+    var displaysetting = document.getElementById('firstcontent10').style.display;
+    if (displaysetting== 'block') {
+        document.getElementById('firstcontent10').style.display = 'none';
+        document.getElementById('firstman10').style.color = "#FFFFFF";
+    }
+    else {
+        document.getElementById('firstcontent10').style.display = 'block';
+        document.getElementById('firstman10').style.color = "#333333";
+    }
 }
-
-    
